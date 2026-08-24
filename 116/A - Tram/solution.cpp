9f92f23748c7bbd53ecdb579c762d346e7bd2a50
Swoyam siddhi pattanayak <swoyamsiddhipattanayak@gmail.com>
2026-08-24 23:37:39 +0530

    Add Tram [A] from Codeforces

---
+++ 116/A - Tram/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    int current = 0;
+    int capacity = 0;
+ 
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+ 
+        current -= a;  // passengers exit
+        current += b;  // passengers enter
+ 
+        capacity = max(capacity, current);
+    }
+ 
+    cout << capacity << endl;
+ 
+    return 0;
+}
